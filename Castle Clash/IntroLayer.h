0dee35b66cdebcf368f2247cd896daa156147b38
Kellie Lynch <kellie@Kellies-MacBook.local>
2012-08-23 14:12:01 -0500

    Initial Commit

---
+++ Castle Clash/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Castle Clash
+//
+//  Created by Kellie Lynch on 8/23/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
